6ab67b72734915d28b4a6c89cd3022fc85bd89e9
cvdafa <zx17343143010@outlook>
2019-11-06 09:54:56 +0800

    init

--- venv/字符串.py
+++ venv/字符串.py
@@ -1,2 +1,10 @@
-t = (a, b)
-print(t)
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+T = (1,)
+print(L[0][0])
+print(L[1][1])
+print(L[2][2])
+print(T)
---
+++ 条件.py
@@ -0,0 +1,6 @@
+age = 16
+if age >= 18:
+    print('是成年人')
+if age <= 18:
+    print('是未成年人')
+print('年龄是：', age)
